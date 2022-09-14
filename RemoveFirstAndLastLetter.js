415d7eaa60b5e74f5ff3a508839b8d8539747063
WestyCodes <110186282+WestyCodes@users.noreply.github.com>
2022-09-14 22:19:53 +0100

    Update RemoveFirstAndLastLetter.js

--- RemoveFirstAndLastLetter.js
+++ RemoveFirstAndLastLetter.js
@@ -3,7 +3,7 @@ It's pretty straightforward. Your goal is to create a function that removes the 
 You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 */
 
-// My Solution
+// My Solution - slice deletes ate specific length values - not index values, I think. Will need to double check.
 function removeChar(str){
   const a = str.slice(1,-1);
   return a
